95ec03a8dc704f5be6f59418854edfbf322feec2
linux08 <abimbola130@gmail.com>
2018-12-05 10:59:06 +0100

    more fix

--- src/ProjectBottomNavigator.js
+++ src/ProjectBottomNavigator.js
@@ -2,41 +2,52 @@ import React from 'react';
 import { Image } from 'react-native';
 import PropTypes from 'prop-types';
 import { createBottomTabNavigator } from 'react-navigation';
-import Inbox from './screens/Project';
-import Profile from './screens/Project';
+import Inbox from './screens/Inbox';
 import Project from './screens/Project';
+import Profile from './screens/Profile';
 import Explore from './screens/ExploreProject';
-import { YELLOW, WHITE } from '../src/utils/constants';
+import { YELLOW } from '../src/utils/constants';
 
 export default createBottomTabNavigator(
     {
         Project,
         Explore,
-        Profile,
         Inbox,
+        Profile,
     },
     {
         defaultNavigationOptions: ({ navigation }) => ({
             tabBarIcon: ({ focused, horizontal, tintColor }) => {
                 const { routeName } = navigation.state;
-                let iconName;
                 if (routeName === 'Project') {
                     return (
-                        <Image resizeMode="contain" source={require('../assets/briefcase.png')}
+                        <Image
+                            resizeMode="contain"
+                            source={require('../assets/briefcase.png')}
                             style={{ tintColor: focused ? YELLOW : `gray` }} />
                     )
                 }
                 else if (routeName === 'Profile') {
-                    return <Image resizeMode="contain" source={require('../assets/inbox.png')}
+                    return (<Image
+                        resizeMode="contain"
+                        source={require('../assets/inbox.png')}
                         style={{ tintColor: focused ? YELLOW : null }} />
+                    )
                 }
                 else if (routeName === 'Explore') {
-                    return <Image resizeMode="contain" source={require('../assets/explore.png')}
-                        style={{ tintColor: focused ? YELLOW : null }} />
+                    return (
+                        <Image
+                            resizeMode="contain"
+                            source={require('../assets/explore.png')}
+                            style={{ tintColor: focused ? YELLOW : null }} />
+                    )
                 }
 
-                return <Image resizeMode="contain" source={require('../assets/profile.png')}
-                    style={{ tintColor: focused ? YELLOW : null }} />
+                return (
+                    <Image resizeMode="contain"
+                        source={require('../assets/profile.png')}
+                        style={{ tintColor: focused ? YELLOW : null }} />
+                )
             },
         }),
         tabBarOptions: {
